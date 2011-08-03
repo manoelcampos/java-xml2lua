7fa013bd1490f72faf96dfb336437accdb9ce403
Manoel Campos da Silva Filho <manoelcampos@gmail.com>
2011-08-03 18:50:25 -0300

    Primeiro commit

---
+++ bin/app.lua
@@ -0,0 +1,10 @@
+---Aplicação para carregar arquivos lua convertidos a partir de um XML e exibir os dados no terminal.
+--@author Manoel Campos da Silva Filho - http://manoelcampos.com
+local produtos = dofile("produtos.lua")
+local destaques = dofile("destaques.lua")
+
+local prod = {}
+for i, dest in pairs(destaques) do
+    prod = produtos[dest.id]
+    print("Id: ", dest.id, "Marca:", prod.marca, "Preço:", prod.preco, "Descrição:", prod.descricao)
+end
---
+++ bin/produtos.lua
@@ -0,0 +1,17 @@
+return  {
+  [12]={
+    descricao = "TV 32''", marca = "Samsung", preco = "1200", 
+  },
+  [150]={
+    descricao = "Netbook", marca = "Asus", preco = "900", 
+  },
+  [198]={
+    descricao = "Impressora Laser", marca = "Samsung", preco = "399", 
+  },
+  [201]={
+    descricao = "Resma de Papel A4 (500 folhas)", marca = "Office", preco = "9", 
+  },
+  [17]={
+    descricao = "Resma de Papel A4 (100 folhas)", marca = "Office", preco = "3", 
+  },
+}
